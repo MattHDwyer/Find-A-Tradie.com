5a938d83fdc2636a46c4431ecfe32de9ad14753e
jian10au <jian10au@gmail.com>
2019-07-30 19:04:30 +1000

    dashboard updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :ratings
   resources :business_profiles
   resources :contacts
+  resources :dashboards
   devise_for :users, :controllers => { :registrations => "registrations" }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
