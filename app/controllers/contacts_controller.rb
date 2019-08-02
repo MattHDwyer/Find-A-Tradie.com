3c9c753038ad4cc50b890350934f27bb3900b3e5
jian10au <jian10au@gmail.com>
2019-08-02 14:17:41 +1000

    testing my way of adding contacts to bp new_contact_path(:params => profile.id)

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,8 +14,9 @@ class ContactsController < ApplicationController
 
   # GET /contacts/new
   def new
+ raise
     @contact = Contact.new
-    
+    # @bp = params[:contact][:business_profile_id]
   end
 
   # GET /contacts/1/edit
@@ -26,7 +27,7 @@ class ContactsController < ApplicationController
   # POST /contacts.json
   def create
     @contact = Contact.new(contact_params)
-
+# raise 
     respond_to do |format|
       if @contact.save
         format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
@@ -70,6 +71,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:email, :mobile_number, :landline_number, :full_address)
+      params.require(:contact).permit(:email, :mobile_number, :landline_number, :full_address, :business_profile_id)
     end
 end
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
 
     def show
-
+        @user = current_user
     end
 
     def all_user_business_profiles
